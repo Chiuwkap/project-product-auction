89fcb352ff6d95f31fd60d45316b86f228b1ad01
Chi Trinh <chikun_88@hotmail.com>
2019-11-26 14:27:35 +0100

    Add Customer Entity

---
+++ src/main/java/project/product/auction/model/Customer.java
@@ -0,0 +1,31 @@
+package project.product.auction.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surename")
+    private String surenName;
+
+    @Column(name = "personNumber")
+    private String personNumber
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+    
+}
