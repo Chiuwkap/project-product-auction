db57ee9cf2b57c04e301a2220dd477a9c25c8e02
Chi Trinh <chikun_88@hotmail.com>
2019-11-26 14:51:50 +0100

    Add Customer Repository

---
+++ src/main/java/project/product/auction/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package project.product.auction.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.product.auction.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<long, Customer> {
+}
