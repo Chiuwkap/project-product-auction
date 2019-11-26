f024cac69d6c33636533171036d4fcb188d2095d
Chi Trinh <chikun_88@hotmail.com>
2019-11-26 15:07:13 +0100

    Skeleton for Controller

--- src/main/java/project/product/auction/repository/CustomerRepository.java
+++ src/main/java/project/product/auction/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package project.product.auction.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.product.auction.model.Customer;
 
-public interface CustomerRepository extends JpaRepository<long, Customer> {
+public interface CustomerRepository extends JpaRepository<Long, Customer> {
 }
