982feb36f48327e97612bd538dc1bd510372e3cd
Olof Kastrup <olofkastrup@gmail.com>
2019-11-29 15:23:54 +0100

    Add endpoint to get all current items

--- src/main/java/project/product/auction/controller/AuctionController.java
+++ src/main/java/project/product/auction/controller/AuctionController.java
@@ -30,4 +30,14 @@ public class AuctionController {
         return itemService.getAllItems();
     }
 
+    @ApiOperation(value = "Get all current items", response = List.class)
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved all items"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+    @GetMapping("/currentauctions")
+    public Iterable<Item> getCurrentAcutions() {
+        return itemService.getAllCurrentItems();
+    }
+
 }
