7c466d0f900202364070637f507d17d83a7f4947
Olof Kastrup <olofkastrup@gmail.com>
2019-12-11 14:15:14 +0100

    Add another test case

--- src/test/java/project/product/auction/AuctionServiceTest.java
+++ src/test/java/project/product/auction/AuctionServiceTest.java
@@ -3,12 +3,11 @@ package project.product.auction;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import project.product.auction.dto.AuctionDto;
 import project.product.auction.dto.BidDto;
 import project.product.auction.model.Bid;
 import project.product.auction.model.Customer;
@@ -44,21 +43,27 @@ public class AuctionServiceTest {
     private AuctionService auctionService;
 
     private Iterable<Item> listOfItems;
+    private Item item1;
     private Item item2;
     private Item item3;
 
     private Customer customer1;
     private Customer customer2;
 
+    private Bid bid1;
+
     @Before
     public void setUp() {
 
+        item1 = new Item(1, "byrå","gustaviansk",BigDecimal.valueOf(10000.00),LocalDateTime.parse("2019-12-01T10:30:00"),"möbler", "http://example.com/byra.png", 10);
         item2 = new Item(2,"iPhone 6","Fint skick!",BigDecimal.valueOf(2000), LocalDateTime.parse("2019-12-10T12:30:00"), "elektronik", "http://example.com/mobil.png", 8);
         item3 = new Item(3,"kofta", "hemmastickad", BigDecimal.valueOf(200.00), LocalDateTime.parse("2019-12-13T15:50:00"), "kläder", "http://example.com/kofta.png", 15);
         Item[] items = {item2, item3};
         listOfItems = Arrays.asList(items);
 
         customer1 = new Customer(2,"ECM","Eric","Cartman","990909-0909","Little Street","99999","South Park","0909-99999999","Cartman@southpark.com");
+
+        bid1 = new Bid(1,1,BigDecimal.valueOf(12000.00), 1, LocalDateTime.parse("2019-11-15T12:30:00"));
     }
 
 
@@ -95,7 +100,7 @@ public class AuctionServiceTest {
         when(bidRepo.findFirstByItemIdOrderByBidCountDesc(bidDto.getItemId())).thenReturn(lastBid);
 
         // Item in database
-        Optional<Item> optionalItem = Optional.of(new Item(1, "byrå","gustaviansk",BigDecimal.valueOf(10000.00),LocalDateTime.parse("2019-12-01T10:30:00"),"möbler", "http://example.com/byra.png", 10));
+        Optional<Item> optionalItem = Optional.of(item1);
 
         // Return start price of item in database
         when(itemRepo.findById(bidDto.getItemId())).thenReturn(optionalItem);
@@ -120,5 +125,19 @@ public class AuctionServiceTest {
         when(customerRepository.save(customer1)).thenReturn(customer1);
         Customer customer = auctionService.registerCustomer(customer1);
         assertNotNull(customer);
+    }
+
+    @Test
+    public void getAuction_test() {
+        long itemId = 1;
+        Optional item = Optional.of(item1);
+        Optional bid = Optional.of(bid1);
+
+        when(itemRepo.findById(itemId)).thenReturn(item);
+        when(bidRepo.findFirstByItemIdOrderByBidCountDesc(itemId)).thenReturn(bid);
+
+        AuctionDto expected = auctionService.getAuction(1);
+
+        assertNotNull(expected);
     }
 }
